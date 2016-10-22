731b26e6b21a0e64d1cf2f7b37aa449b92ccbf90
yue82 <no.82.vuf.lab@gmail.com>
2016-10-23 03:50:01 +0900

    fix cross entropy err for mini batch

--- ch04/loss_funcs.py
+++ ch04/loss_funcs.py
@@ -7,5 +7,9 @@ def mean_squared_error(y, t):
 
 
 def cross_entropy_error(y, t):
-    delta = 1e-7
-    return -np.sum(t * np.log(y + delta))
+    if y.ndim == 1:
+        t = t.reshape(1, t.size)
+        y = y.reshape(1, y.size)
+
+    batch_size = y.shape[0]
+    return -np.sum(np.log(y[np.arange(batch_size), t])) / batch_size
